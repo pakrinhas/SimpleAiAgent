2a46d8fe8708426aaa3ec00d022a1d62d9c4e46f
Pakrinha Sim <pakrinha124@gmail.com>
2026-02-04 21:30:04 -0700

    Finished abit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain_openai import  ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()  # Load environment variables from .env file
 
@@ -17,3 +18,28 @@ class ResearchResponse(BaseModel):
 
 llm = ChatAnthropic(model="claude-sonnet-4-5-20250929")  # Example model name
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that gathers information on various topics using different tools.
+            Answer the user query and use neccessary tools.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[], 
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "Research the impact of climate change on polar bear populations."})
+print(raw_response)
